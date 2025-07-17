299f0a579a2a7297e6d4c29a23ea0d54588b60cd
GFSA66 <gf66sorokina@gmail.com>
2025-07-17 18:03:02 -0500

    1.3.2

--- my_project/members/models.py
+++ my_project/members/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# class reg_form(models.Model):
+#     username = models.CharField(label='username', max_length=64)
+#     password = models.CharField(widget=models.PasswordInput)
+#     email = models.EmailField(label="email", max_length=64)
+    
+#     def __str__(self):
+#         return self.username
--- my_project/members/views.py
+++ my_project/members/views.py
@@ -25,6 +25,7 @@ def logout_user(request):
     messages.success(request, ("You Have Been Logged out!"))
     return redirect('home')
 
+# Добавить email в форму регистрации
 def register_user(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
