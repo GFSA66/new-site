3ef2eecbc9f67352dc307e26935f1c5a45c10b3a
GFSA66 <gf66sorokina@gmail.com>
2025-07-19 23:57:19 +0300

    1.5.1

--- my_project/home/views.py
+++ my_project/home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'home/home.html')
+
+def profile_page(request):
+    return render(request, 'home/profile.html')
